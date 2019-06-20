46afad3738569729a25e37e99d46074b8f85b5f1
shiweihappy <shi_weihappy@126.com>
2019-06-20 14:52:07 +0800

    add new files

--- source/conf.py
+++ source/conf.py
@@ -14,8 +14,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('./ee'))
-import sphinx_rtd_theme
+sys.path.insert(0, os.path.abspath('./../ee'))
+# import sphinx_rtd_theme
 
 
 # -- Project information -----------------------------------------------------
@@ -76,8 +76,9 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = 'alabaster'
+# html_theme = 'sphinx_rtd_theme'
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
